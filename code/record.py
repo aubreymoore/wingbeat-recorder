9191620aa324fd963b65d598273f8dd4e58f59cd
aubreymoore <aubreymoore@guam.net>
2025-04-23 10:04:17 +0800

    safety

--- code/record.py
+++ code/record.py
@@ -8,32 +8,57 @@ import os
 # ensure output directories exist
 os.makedirs(WAVFILE_DIR, exist_ok=True)
 os.makedirs(WAVEFORM_DIR, exist_ok=True)
-os.makedirs(WAVFILE_PLOT_DIR, exist_ok=True)
+
+# ensure wavfile.csv exists
+wavefile_csv_path = f'{DATA_DIR}/wavfile.csv'
+if not os.path.exists(wavefile_csv_path):
+    with open(wavefile_csv_path, 'w') as f:
+        f.write('start_time,min,max,mean,std,num_waveforms\n') # header row
+
+wbrec.display_config()
 
 # run 1s test recording
-wbrec.display_config()
 wbrec.run_test_recording()
 print()
 
 # run recording session
 for i in range(NRECORDINGS):
+    print(i+1)
     
     # recording
     start_time = time.time()    
     wavfile_path = wbrec.record(DEVICE, DURATION, FORMAT, CHANNELS, WAVFILE_DIR)
     end_time = time.time()
     recording_time = end_time - start_time
+    print(f'  recording time: {recording_time:.2f}s')
 
-    # analyze
+    # extract waveforms
     start_time = time.time()
-    wbrec.extract_waveforms(wavfile_path, WAVEFORM_DIR, SAVE_WAVFILE_PLOTS, PEAK_THRESHOLD, WAVEFORM_WIDTH, WAVFILE_PLOT_DIR)
+    print(f'  extracting waveforms from {wavfile_path}')
+    results = wbrec.extract_waveforms(wavfile_path, WAVEFORM_DIR, PEAK_THRESHOLD, WAVEFORM_WIDTH)
     end_time = time.time()
-    analysis_time = end_time - start_time
+    processing_time = end_time - start_time
+    print(f'  processing time: {processing_time:.2f}s')
+    
+    # If DEL_WAV is True, delete the recording immediately after extracting waveforms
+    if DEL_WAV:
+        # delete wav file
+        print(f'  deleting {wavfile_path}')
+        os.remove(wavfile_path)
+    
+    # append a line to wavfile.csv
+    with open(wavefile_csv_path, 'a') as f:
+        start_time = os.path.basename(wavfile_path).replace('.wav', '')
+        s = f'{start_time},{results["min"]},{results["max"]},{results["mean"]},{results["std"]},{results["num_waveforms"]}\n'
+        print(f'  appending data to wavefile.csv: {s.strip()}')
+        f.write(s)
     
     # sleep until time for next recording cycle
-    sleeptime = int(CYCLETIME - (recording_time + analysis_time))
-    if sleeptime >= 0:
-        print(f'  sleeping for {sleeptime}s before starting next recording')
-        time.sleep(sleeptime)
     
+    if i < NRECORDINGS - 1:
+        sleeptime = int(CYCLETIME - (recording_time + processing_time))
+        if sleeptime >= 0:
+            print(f'  sleeping for {sleeptime}s before starting next recording')
+            time.sleep(sleeptime)
+        
 print('\nfinished')
--- code/wbrec.py
+++ code/wbrec.py
@@ -8,7 +8,8 @@ import time
 from scipy.io import wavfile
 from scipy.signal import find_peaks
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from icecream import ic
 
 
 def record(device, duration, format, channels, wavfile_dir):
@@ -19,11 +20,11 @@ def record(device, duration, format, channels, wavfile_dir):
     return wavfile_path
 
 
-def extract_waveforms(wavfile_path: str, waveform_dir: str, plot_peaks: bool, peak_threshold: float, waveform_width: int, wavfile_plot_dir: str)->None:
+def extract_waveforms(wavfile_path: str, waveform_dir: str, peak_threshold: float, waveform_width: int):
     """
     Extract waveforms (transients) from a wav file and save them as separate wav files.
     """
-    print(f'  extracting waveforms from {wavfile_path}')
+    # print(f'  extracting waveforms from {wavfile_path}')
     
     # Calculate the height parameter for the find_peaks function
     sr, data = wavfile.read(wavfile_path)
@@ -32,17 +33,19 @@ def extract_waveforms(wavfile_path: str, waveform_dir: str, plot_peaks: bool, pe
     height = mean + peak_threshold * std    
     peaks, _ = find_peaks(data, height=height, distance=waveform_width)
     print(f'    found {len(peaks)} peaks')
+    results = {'mean': mean, 'std': std, 'min': np.min(data), 'max': np.max(data)}
     
+    num_waveforms = 0
     for peak in peaks:
         start = peak - waveform_width // 2
         stop = peak + waveform_width // 2
         if start >= 0 and stop <= len(data):
             waveform = data[start:stop]
             
-            # Calculate time between the start of the wav file and the start of the transient
+            # Calculate time between the start of the wav file and the start of the transient waveform
             seconds = start/sr
             
-            # Calculate the new timestamp and use this to name the wav file for the transient
+            # Calculate the new timestamp and use this to name the wav file for the transient waveform
             timestamp_str = wavfile_path.split('/')[-1].replace('.wav', '')
               
             # Extract the timestamp from the wav file name
@@ -54,19 +57,24 @@ def extract_waveforms(wavfile_path: str, waveform_dir: str, plot_peaks: bool, pe
             # Create the new wav file name
             waveform_file_path = f'{waveform_dir}/{waveform_timestamp.isoformat()}.wav'
             
-            # Save the transient as a wav file
+            # Save the transient waveform as a wav file
             wavfile.write(waveform_file_path, sr, waveform)
+            
+            num_waveforms += 1
+    results['num_waveforms'] = num_waveforms
         
-    if plot_peaks:
-        plt.plot(data)
-        plt.plot(peaks, data[peaks], 'x')
-        plt.plot(np.zeros_like(data), '--', color='gray')
-        plt.savefig(f'{wavfile_plot_dir}/{os.path.basename(wavfile_path).replace(".wav", ".png")}')
-        plt.close()
+    return results
+        
+    # if plot_peaks:
+    #     plt.plot(data)
+    #     plt.plot(peaks, data[peaks], 'x')
+    #     plt.plot(np.zeros_like(data), '--', color='gray')
+    #     plt.savefig(f'{wavfile_plot_dir}/{os.path.basename(wavfile_path).replace(".wav", ".png")}')
+    #     plt.close()
 
-# wavfile_path = 'wavfiles/2025-04-12T14:48:12+08:00.wav'     
-# transient_width = 10000  
-# extract_transients(wavfile_path=wavfile_path, transient_dir='transients',  plot_peaks=False, transient_width=transient_width)
+# wavfile_path = '/home/aubrey/Desktop/wingbeat-recorder/data/recordings/2025-04-22T17:29:41.225735+08:00.wav'     
+# results = extract_waveforms(wavfile_path, WAVEFORM_DIR, PEAK_THRESHOLD, WAVEFORM_WIDTH)
+# ic(results)
 
 
 def analyze(wavfile):
@@ -106,10 +114,10 @@ def display_config():
     with open('code/config.py', 'r') as f:
         lines = f.readlines()
         for line in lines:
-            if line[0] != '#':
+            if not line.startswith(('#', '\n')):
                 print(line.strip())
     print()
             
-# display_config()
+display_config()
 
     
